b0ea3c0ff49ac0310be0716a484e0c7aaac08502
NikhilMaalige <nmaalige@gmail.com>
2026-04-20 14:20:44 -0400

    Part 3 Done

--- Lexer.java
+++ Lexer.java
@@ -4,26 +4,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
-	
+
 	/*
 	 * A lexer (or "tokenizer") converts an input into tokens that
 	 * eventually need to be interpreted.
 	 * 
-	 * Given the input 
-	 *    (\bat  .bat flies)cat  λg.joy! )
+	 * Given the input
+	 * (\bat .bat flies)cat λg.joy! )
 	 * you should output the ArrayList of strings
-	 *    [(, \, bat, ., bat, flies, ), cat, \, g, ., joy!, )]
+	 * [(, \, bat, ., bat, flies, ), cat, \, g, ., joy!, )]
 	 *
 	 */
 	public ArrayList<String> tokenize(String input) {
 		ArrayList<String> tokens = new ArrayList<String>();
 
-		// This next line is definitely incorrect!
-		tokens.add(input);
+		int counter = 0;
+		String temp = "";
+		while (counter < input.length() && input.charAt(counter) != ';') {
+			char current = input.charAt(counter);
 
+			 if (current == '\\' || current == 'λ' || current == '(' || current == ')' || current == '.' || current == '=' || current == ' ') {
+				if (!temp.isEmpty()) {
+					tokens.add(temp);
+					temp = "";
+				}
+				if(current != ' '){
+					if(current == 'λ') {
+						tokens.add("\\");
+					}
+					else {
+						tokens.add(Character.toString(current));
+					}
+				}
+				counter++;
+			} else {
+				temp += Character.toString(current);
+				counter++;
+			}
+		}
+		if (!temp.isEmpty()) {
+			tokens.add(temp);
+		}
+		System.out.println(tokens);
 		return tokens;
 	}
-
-
 
 }
